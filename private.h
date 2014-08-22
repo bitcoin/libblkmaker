d46062c7e66bd57e5e2640c4289c127c150bb495
Luke Dashjr <luke-jr+git@utopios.org>
2014-08-22 19:07:56 +0000

    Use libbase58 for base58 decoding

--- private.h
+++ private.h
@@ -14,10 +14,6 @@ extern void _blktxn_free(struct blktxn_t *);
 extern void _blkmk_bin2hex(char *out, const void *data, size_t datasz);
 extern bool _blkmk_hex2bin(void *o, const char *x, size_t len);
 
-// base58.c
-extern bool _blkmk_b58tobin(void *bin, size_t binsz, const char *b58, size_t b58sz);
-extern int _blkmk_b58check(void *bin, size_t binsz, const char *b58);
-
 // inline
 
 // NOTE: This must return 0 for 0
