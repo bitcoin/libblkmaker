54cab994237a920237321eea80c3d26f395e0d67
Luke Dashjr <luke-jr+git@utopios.org>
2015-02-08 05:44:56 +0000

    Support for BIP66 version 3 blocks

--- blkmaker.h
+++ blkmaker.h
@@ -8,7 +8,7 @@
 #include <blktemplate.h>
 
 #define BLKMAKER_VERSION (5L)
-#define BLKMAKER_MAX_BLOCK_VERSION (2)
+#define BLKMAKER_MAX_BLOCK_VERSION (3)
 
 extern bool (*blkmk_sha256_impl)(void *hash_out, const void *data, size_t datasz);
 
